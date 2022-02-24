a548c5275d5d524432cd0ea56d6fbd189ef2c7e9
norseninja <espenotl@stud.ntnu.no>
2022-02-24 20:41:40 +0100

    refactoring of schema-postgres.sql

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,28 +1,3 @@
-CREATE TABLE IF NOT EXISTS "user" (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    permission_id INTEGER,
-    enabled BOOLEAN
-    );
-
-CREATE TABLE IF NOT EXISTS product (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    description VARCHAR(255),
-    origin VARCHAR(255),
-    price DOUBLE PRECISION,
-    category_id INTEGER,
-    inventory_id INTEGER,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
-);
-
 CREATE TABLE IF NOT EXISTS product_category (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
@@ -40,6 +15,19 @@ CREATE TABLE IF NOT EXISTS product_inventory (
     deleted_at TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    origin VARCHAR(255),
+    price DOUBLE PRECISION,
+    fk_category_id INTEGER REFERENCES product_category (id),
+    fk_inventory_id INTEGER REFERENCES product_inventory (id),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS permission_level (
     id SERIAL PRIMARY KEY,
     admin_type VARCHAR(255),
@@ -47,3 +35,15 @@ CREATE TABLE IF NOT EXISTS permission_level (
     created_at TIMESTAMP,
     updated_at TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS "user" (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    fk_permission_id INTEGER REFERENCES permission_level (id),
+    enabled BOOLEAN
+    );
