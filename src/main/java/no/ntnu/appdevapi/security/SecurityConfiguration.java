26fde059b02af9aa946775b5b1798adcebea8e07
norseninja <espenotl@stud.ntnu.no>
2022-06-08 22:19:10 +0200

    Removed unused imports & minor refactoring

--- src/main/java/no/ntnu/appdevapi/security/SecurityConfiguration.java
+++ src/main/java/no/ntnu/appdevapi/security/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,13 +15,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
-
 
 /**
  * Creates AuthenticationManager - set up authentication type
@@ -38,11 +30,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Autowired
   private UnauthorizedEntryPoint unauthorizedEntryPoint;
 
-  private String admin = "admin";
-  private String user = "user";
-  private String owner = "owner";
-
-
   @Override
   public void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
@@ -53,10 +40,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
    * Configure the authorization rules
    *
    * @param http HTTP Security object
-   * @throws Exception
    */
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    final String admin = "admin";
+    final String owner = "owner";
+
     http.cors().and().csrf().disable();
     http.sessionManagement().sessionCreationPolicy(STATELESS);
     http.authorizeRequests()
